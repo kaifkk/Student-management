335f17d1aff53bfee59ce2cb254d1aa6dc791ffa
tonmoyyy <kamrulhasantonmoy9@gmail.com>
2026-07-20 20:39:11 +0600

    Connected dashboard with statistics page

--- src/gui/dashboard.py
+++ src/gui/dashboard.py
@@ -2,6 +2,11 @@ from tkinter import Tk, Label, Button, messagebox
 
 from src.managers.student_manager import StudentManager
 from src.gui.add_student_page import AddStudentPage
+from src.gui.view_student_page import ViewStudentsPage
+from src.gui.search_student_page import SearchStudentPage
+from src.gui.update_student_page import UpdateStudentPage
+from src.gui.delete_student_page import DeleteStudentPage
+from src.gui.statistics_page import StatisticsPage
 
 
 class DashboardApp(Tk):
@@ -21,7 +26,7 @@ class DashboardApp(Tk):
         Button(self, text="Search Student", command=self.open_search_student).pack(pady=5)
         Button(self, text="Update Student", command=self.open_update_student).pack(pady=5)
         Button(self, text="Delete Student", command=self.open_delete_student).pack(pady=5)
-        Button(self, text="Statistics", command=self.not_implemented).pack(pady=5)
+        Button(self, text="Statistics", command=self.open_statistics).pack(pady=5)
         Button(self, text="Exit", command=self.destroy).pack(pady=5)
 
     def update_total_students(self):
@@ -30,8 +35,8 @@ class DashboardApp(Tk):
     def open_add_student(self):
         self.withdraw()
         AddStudentPage(self, self.manager)
-    
-        def open_view_students(self):
+
+    def open_view_students(self):
         self.withdraw()
         ViewStudentsPage(self, self.manager)
 
@@ -46,6 +51,10 @@ class DashboardApp(Tk):
     def open_delete_student(self):
         self.withdraw()
         DeleteStudentPage(self, self.manager)
+
+    def open_statistics(self):
+        self.withdraw()
+        StatisticsPage(self, self.manager)
 
     def not_implemented(self):
         messagebox.showinfo("Coming soon", "This feature belongs to another module.")
